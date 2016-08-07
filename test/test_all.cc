e5f47001ef51ceed3ab78b9606fef723f5dcdff6
Sang Rhee <leesy579@gmail.com>
2016-08-07 23:28:22 +0900

    cleaned up test.

--- test/test_all.cc
+++ test/test_all.cc
@@ -10,16 +10,16 @@ char* buffer;
 double* durations;
 
 void test_write(long long unsigned int, int, size_t, int);
-void test_nvm_write(long long unsigned int, int, size_t, int);
+void test_durable_write(long long unsigned int, int, size_t, int);
+void test_nvm_durable_write(long long unsigned int, int, size_t, int);
 void remove_files(int);
 
 void test_write_performance(void (*test_func)(long long unsigned int, int, size_t, int) )
 {
     int nthread;
 
-    system("clear");
- 
     /* write (WRITE_BYTES1) bytes at a time */
+    printf("[Writing %d bytes at a time totaling %llu bytes]\n", WRITE_BYTES1, TOTAL_FILE_SIZE);
     for(nthread = 1; nthread <= MAX_THREADS; nthread*=2) {
         printf("#-------- %d threads APPEND TEST -------\n", nthread);
         (*test_func)(TOTAL_FILE_SIZE, nthread, WRITE_BYTES1, _WRITE_APPEND_);
@@ -29,13 +29,15 @@ void test_write_performance(void (*test_func)(long long unsigned int, int, size_
         remove_files(nthread);
     }
 
+    printf("\n\n**PRESS ENTER TO CONTINUE TO NEXT TEST**\n\n");
     getchar();
-    system("clear");
 
     /* write (WRITE_BYTES2) bytes at a time */
+    printf("[Writing %d bytes at a time totaling %llu bytes]\n", WRITE_BYTES2, TOTAL_FILE_SIZE);
     for(nthread = 1; nthread <= MAX_THREADS; nthread*=2) {
         printf("#-------- %d threads APPEND TEST -------\n", nthread);
         (*test_func)(TOTAL_FILE_SIZE, nthread, WRITE_BYTES2, _WRITE_APPEND_);
+//        printf("#-------------- RANDOM TEST ---------------------\n");
 //        (*test_func)(TOTAL_FILE_SIZE, nthread, WRITE_BYTES2, _WRITE_RANDOM_);
         printf("\n");
         remove_files(nthread);
@@ -44,10 +46,25 @@ void test_write_performance(void (*test_func)(long long unsigned int, int, size_
 
 int main()
 {
-    printf("\n[nvm write test]\n");
-    test_write_performance(test_nvm_write);
-    remove_files(MAX_THREADS);
+    system("clear");
+
+    //////////NVM durable write//////////
+    printf("\n[nvm durable write test]\n");
+    test_write_performance(test_nvm_durable_write);
+
+    printf("\n\n**PRESS ENTER TO CONTINUE TO NEXT TEST**\n\n");
+    getchar();
+    system("clear");
+
+    //////////write//////////
     printf("\n[write test]\n");
     test_write_performance(test_write);
-    remove_files(MAX_THREADS);
+
+    printf("\n\n**PRESS ENTER TO CONTINUE TO NEXT TEST**\n\n");
+    getchar();
+    system("clear");
+
+    //////////durable write//////////
+    printf("\n[durable write test]\n");
+    test_write_performance(test_durable_write);
 }
