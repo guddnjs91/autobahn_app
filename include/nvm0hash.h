c0aae87fbe3f035f74afdbef45228b8c141466d5
Sang Rhee <leesy579@gmail.com>
2016-08-08 21:41:50 +0900

    fixed linkedlist.

--- include/nvm0hash.h
+++ include/nvm0hash.h
@@ -5,22 +5,17 @@
 #include "nvm0list.h"
 #include <unordered_map>
 
-#define HASH_NODE_VALID 0
-#define HASH_NODE_INVALID 1
-
 struct hash_node
 {
     struct inode_entry* inode;
-    int                 valid;
+    bool                is_valid;
     struct hash_node*   prev;
     struct hash_node*   next;
 };
 
 struct hash_table
 {
-    std::unordered_map<uint32_t, struct hash_node*> map;
-    int count_total;
-    int count_invalid;
+    std::unordered_map<uint32_t, struct hash_node*> map; //key = lbn
     struct list* invalid_list;
 };
 
